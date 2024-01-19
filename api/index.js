1a88afc5c7cc770305dd4276aa5fd4b69b109b71
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-01-19 02:35:05 +0100

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("sever is running on port 3000");
 });
 
-app.use("/api/user", router);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    emails: {
+    email: {
       type: String,
       required: true,
       unique: true,

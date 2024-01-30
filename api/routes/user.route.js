06fcd17931fc44392f20f3aff9fe57610e023cfb
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-01-30 11:17:46 +0100

    show post comments

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
+  deleteUser,
+  getUser,
+  getUsers,
+  signout,
   test,
   updateUser,
-  deleteUser,
-  signout,
-  getUsers,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,4 +16,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
+
 export default router;

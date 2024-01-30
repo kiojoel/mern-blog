c37e52b1b875893b84de9e0eb7d491e6b0c4d718
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-01-30 15:19:24 +0100

    complete project and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,36 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Joel's Blog! It's a personal project where Joel shares
+              thoughts and ideas about technology, coding, and more. Joel, a
+              passionate developer, loves to write and connect with fellow
+              developers and the world.
+            </p>
+
+            <p>
+              Explore weekly articles and tutorials on web development, software
+              engineering, and programming languages here on the blog. Joel is a
+              perpetual learner and tech enthusiast, consistently delves into
+              new technologies. Keep coming back for new content!
+            </p>
+
+            <p>
+              Feel free to share your thoughts by leaving comments on our posts
+              and interacting with fellow readers. Give a thumbs up to comments
+              you like and join the conversation by replying. We believe in
+              fostering a community of learners who can support each other's
+              growth and improvement. Your engagement is highly valued!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,13 @@
+import CallToAction from "../componenets/CallToAction";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Pojects</h1>
+      <p className="text-md text-gray-500">
+        Simplifying engaging & interesting JavaScript documentation topics
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
